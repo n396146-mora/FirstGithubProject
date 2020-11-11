57d39be284d49e99e2d78c48b59aa9ecb7b9cb8b
Glen Mora <glenmora@gmail.com>
2020-11-11 08:02:00 -0500

    Initial commit

---
+++ DoThis.py
@@ -0,0 +1,5 @@
+# in this file, print your name and ask the user for two numbers,
+# then print the larger between the two:  
+# pi to the power of the first number 
+# or the square root of the second number.
+# then commit and push it back up to github.
