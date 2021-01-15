5c9769e2af04cd61a1d678a7d25ceac28dbcd81c
n396146-mora <74068106+n396146-mora@users.noreply.github.com>
2021-01-15 12:10:52 -0500

    Update DoThis.py

--- DoThis.py
+++ DoThis.py
@@ -2,3 +2,8 @@
 # ask the user for two numbers they want added,
 # add them together and return the result. 
 # then commit and push it back up to github.
+print("matthew")
+x=int(input("Enter the first number you want added: "))
+y=int(input("Enter the second number you want added: "))
+sum=x+y
+print("The sum is " + str(sum))
